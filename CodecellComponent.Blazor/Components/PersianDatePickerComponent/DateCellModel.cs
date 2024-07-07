bce42542cd787278aa371a4fca4d0976def1b9f1
Sohrab Gheisari <sohrabgheisari@hotmail.com>
2024-07-07 14:03:17 +0330

    Add project files.

---
+++ CodecellComponent.Blazor/Components/PersianDatePickerComponent/DateCellModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CodecellComponent.Blazor.Components.PersianDatePickerComponent
+{
+    public class DateCellModel
+    {
+        public int Day { get; set; }
+        public DateTime Date { get; set; }
+        public bool Show => Day > 0;
+        public string PersianDateFormat => PersianDate();
+        string PersianDate()
+        {
+            if (Day <= 0) return string.Empty;
+            System.Globalization.PersianCalendar pc = new();
+            var year = pc.GetYear(Date);
+            var month = pc.GetMonth(Date);
+            var day = pc.GetDayOfMonth(Date);
+
+            return $"{year}/{month.ToString("D2")}/{day.ToString("D2")}";
+        }
+    }
+}
---
+++ CodecellComponent.Blazor/Components/PersianDatePickerComponent/PersianDateHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace CodecellComponent.Blazor.Components.PersianDatePickerComponent
+{
+    public static class PersianDateHelper
+    {
+
+        public static string[] WeekNames => ["ش", "ی", "د", "س", "چ", "پ", "ج"];
+        public static int GetWeekSpan(this DayOfWeek week)
+        {
+            return week switch
+            {
+                DayOfWeek.Saturday => 0,
+                DayOfWeek.Sunday => 1,
+                DayOfWeek.Monday => 2,
+                DayOfWeek.Tuesday => 3,
+                DayOfWeek.Wednesday => 4,
+                DayOfWeek.Thursday => 5,
+                DayOfWeek.Friday => 6,
+                _ => 0
+            };
+        }
+
+        public static string GetWeekName(this DayOfWeek week) =>
+             week switch
+             {
+                 DayOfWeek.Saturday => "شنبه",
+                 DayOfWeek.Sunday => "یکشنبه",
+                 DayOfWeek.Monday => "دوشنبه",
+                 DayOfWeek.Tuesday => "سه شنبه",
+                 DayOfWeek.Wednesday => "چهارشنبه",
+                 DayOfWeek.Thursday => "پنجشنبه",
+                 DayOfWeek.Friday => "جمعه",
+                 _ => "نامشخص"
+             };
+
+        public static string GetMonthName(this int month) =>
+              month switch
+              {
+                  1 => "فروردین",
+                  2 => "اردیبهشت",
+                  3 => "خرداد",
+                  4 => "تیر",
+                  5 => "مرداد",
+                  6 => "شهریور",
+                  7 => "مهر",
+                  8 => "آبان",
+                  9 => "آذر",
+                  10 => "دی",
+                  11 => "بهمن",
+                  12 => "اسفند",
+                  _ => "نامشخص",
+              };
+        public static (int MonthNumber, string MonthName)[] GetMonths() =>
+            [
+               (1, "فروردین"),
+               (2, "اردیبهشت"),
+               (3, "خرداد"),
+               (4, "تیر"),
+               (5, "مرداد"),
+               (6, "شهریور"),
+               (7, "مهر"),
+               (8, "آبان"),
+               (9, "آذر"),
+               (10, "دی"),
+               (11, "بهمن"),
+               (12, "اسفند")
+            ];
+
+    }
+}
